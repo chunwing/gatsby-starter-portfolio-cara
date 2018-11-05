cecc096438c03893653f498e54e55dd09323eef8
Wing Ng <Wing.Ng@isobar.com>
2018-11-05 17:39:08 +0800

    add/react-light-box

--- src/components/ProjectCard.jsx
+++ src/components/ProjectCard.jsx
@@ -1,9 +1,15 @@
 /* global tw */
-import React from 'react';
+import React, {
+  Component
+} from 'react';
 import PropTypes from 'prop-types';
 import styled from 'react-emotion';
+import Lightbox from 'react-image-lightbox';
+import 'react-image-lightbox/style.css'; // This only needs to be imported once in your app
 
-const Wrapper = styled.a`
+// const { photoIndex, isOpen } = this.state;
+
+const Wrapper = styled.button `
   width: 100%;
   ${tw('shadow-lg relative no-underline rounded-lg px-8 py-8 md:py-24 text-white')};
   background: ${props => props.bg};
@@ -13,28 +19,96 @@ const Wrapper = styled.a`
   }
 `;
 
-const Text = styled.div`
+const Text = styled.div `
   ${tw('opacity-75 font-sans text-sm md:text-base')};
   text-shadow: 0 2px 10px rgba(0, 0, 0, 0.3);
 `;
 
-const Title = styled.div`
+const Title = styled.div `
   ${tw('text-white uppercase text-2xl md:text-3xl xl:text-4xl tracking-wide font-sans pt-8')};
   text-shadow: 0 2px 10px rgba(0, 0, 0, 0.3);
 `;
 
-const ProjectCard = ({ title, link, children, bg }) => (
-  <Wrapper href={link} target="_blank" rel="noopener noreferrer" bg={bg}>
-    <Text>{children}</Text>
-    <Title>{title}</Title>
-  </Wrapper>
-);
 
-export default ProjectCard;
+export default class ProjectCard extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      photoIndex: 0,
+      isOpen: false,
+    };
+
+
+
+  }
+  render() {
+    const {
+      photoIndex,
+      isOpen
+    } = this.state;
+    const images = this.props.src;
+    return ( <
+      Wrapper onClick = {
+        () => this.setState({
+          isOpen: true
+        })
+      }
+      rel = "noopener noreferrer"
+      bg = {
+        this.props.bg
+      } >
+      <
+      Text > {
+        this.props.children
+      } < /Text> <
+      Title > {
+        this.props.title
+      } < /Title> {
+        isOpen && ( <
+          Lightbox mainSrc = {
+            images[photoIndex]
+          }
+          nextSrc = {
+            images[(photoIndex + 1) % images.length]
+          }
+          prevSrc = {
+            images[(photoIndex + images.length - 1) % images.length]
+          }
+          onCloseRequest = {
+            () => this.setState({
+              isOpen: false
+            })
+          }
+          onMovePrevRequest = {
+            () =>
+            this.setState({
+              photoIndex: (photoIndex + images.length - 1) % images.length,
+            })
+          }
+          onMoveNextRequest = {
+            () =>
+            this.setState({
+              photoIndex: (photoIndex + 1) % images.length,
+            })
+          }
+          />
+        )
+      } <
+      /Wrapper>
+
+
+
+
+
+    );
+  }
+}
 
 ProjectCard.propTypes = {
   title: PropTypes.string.isRequired,
-  link: PropTypes.string.isRequired,
+  // link: PropTypes.string.isRequired,
+  src: PropTypes.arrayOf(PropTypes.string).isRequired,
   children: PropTypes.oneOfType([PropTypes.string, PropTypes.element]).isRequired,
   bg: PropTypes.string.isRequired,
 };
